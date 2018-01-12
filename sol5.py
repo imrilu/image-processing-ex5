ca9d6b87f0b0228f455b6bb56107f4ea00a0a28a
imrilu <imri.luzzattov@gmail.com>
2018-01-12 18:08:22 +0400

    last changes

--- sol5.py
+++ sol5.py
@@ -1,9 +1,7 @@
-from functools import lru_cache
 import numpy as np
 from scipy.misc import imread as imread
 import scipy.ndimage as ndimage
 from skimage.color import rgb2gray
-from scipy import signal as signal
 import matplotlib.pyplot as plt
 from keras.layers import Input, Convolution2D, Activation, merge
 from keras.models import Model
@@ -11,8 +9,11 @@ from keras.optimizers import Adam
 import random
 import sol5_utils
 
+
 im_cache = {}
-def read_image(filename, representation):
+corrupt_cache = {}
+
+def read_image(filename, representation=1):
     """
     A method for reading an image from a path and loading in as gray or in color
     :param filename: The path for the picture to be loaded
@@ -22,25 +23,11 @@ def read_image(filename, representation):
     """
     im = imread(filename)
     if representation == 1:
-        # converting to gray
-        im = rgb2gray(im) / 255
-    else:
-        if representation == 2:
-            im = im.astype(np.float64)
-            # setting the image's matrix to be between 0 and 1
-            im = im / 255
-    im_cache[filename] = im
+        im = rgb2gray(im)
+    elif representation == 2:
+        im = (im / 255).astype(np.float32)
+    im = im.astype(np.float64)
     return im
-
-
-def strech_helper(im):
-    """
-    A helper function for stretching the image
-    :param image: The input picture to be stretched
-    :param hist: The histogram of the input image
-    :return: The stretched image
-    """
-    return (im - np.min(im))/(np.max(im) - np.min(im))
 
 
 def load_dataset(filenames, batch_size, corruption_func, crop_size):
@@ -53,28 +40,22 @@ def load_dataset(filenames, batch_size, corruption_func, crop_size):
     :return: generator, yielding each time a set of corrupted/original image patches
     """
     while True:
-        # corrupted pics
-        source_batch = np.zeros(shape=(batch_size, 1, crop_size[0], crop_size[1]), dtype=np.float64)
-        # orig pics
-        target_batch = np.zeros(shape=(batch_size, 1, crop_size[0], crop_size[1]), dtype=np.float64)
-        random_pts = np.random.choice(len(filenames), batch_size)
-
-        for i in range(len(random_pts)):
-            isCached = im_cache.get(filenames[random_pts[i]], False)
-            # checking cache
-            if isCached is False:
-                im = read_image(filenames[random_pts[i]], 1)
-            else:
-                im = im_cache[filenames[random_pts[i]]]
-            # getting random crop top left pts based on dimension of pic
-            crop_top_left_corner = [np.random.choice(im.shape[0] - crop_size[0], 1),
-                                    np.random.choice(im.shape[1] - crop_size[1], 1)]
-            org_corrupt = corruption_func(im)[int(crop_top_left_corner[0]):int(crop_top_left_corner[0]+crop_size[0]),
-                         int(crop_top_left_corner[1]):int(crop_top_left_corner[1]+crop_size[1])]
-            cropped_im = im[int(crop_top_left_corner[0]):int(crop_top_left_corner[0]+crop_size[0]),
-                         int(crop_top_left_corner[1]):int(crop_top_left_corner[1]+crop_size[1])]
-            source_batch[i][0] = org_corrupt - 0.5
-            target_batch[i][0] = cropped_im - 0.5
+        height, width = crop_size
+        # creating the array for the source and target batches
+        source_batch = np.empty((batch_size, 1, height, width))
+        target_batch = np.empty((batch_size, 1, height, width))
+        for i in range(batch_size):
+            file = random.choice(filenames)
+            if file not in im_cache.keys():
+                im_cache[file] = read_image(file)
+                corrupt_cache[file] = corruption_func(im_cache[file])
+            # choosing the top left corner of the patch to extract
+            rand_col = random.randint(0, im_cache[file].shape[1] - width)
+            rand_row = random.randint(0, im_cache[file].shape[0] - height)
+            target_batch[i, 0, :, :] = im_cache[file][rand_row:rand_row + height,
+                                       rand_col: rand_col + width] - 0.5
+            source_batch[i, 0, :, :] = corrupt_cache[file][rand_row:rand_row + height,
+                                       rand_col: rand_col + width] - 0.5
 
         yield source_batch, target_batch
 
@@ -86,9 +67,9 @@ def resblock(input_tensor, num_channels):
     :param num_channels: number of channels for the model
     :return: a ResNet block
     """
-    res = Convolution2D(num_channels, 3, 3, border_mode='same', dim_ordering='th')(input_tensor)
+    res = Convolution2D(num_channels, 3, 3, border_mode='same')(input_tensor)
     res = Activation('relu')(res)
-    res = Convolution2D(num_channels, 3, 3, border_mode='same', dim_ordering='th')(res)
+    res = Convolution2D(num_channels, 3, 3, border_mode='same')(res)
     # TODO: check if merge/Add is the correct method for adding 2 tensors
     final = merge([input_tensor, res], mode='sum')
     return final
@@ -104,16 +85,15 @@ def build_nn_model(height, width, num_channels, num_res_blocks):
     :return: A model as seen in pdf
     """
     a = Input(shape=(1, height, width))
-    model = Convolution2D(num_channels, 3, 3, border_mode='same', dim_ordering='th')(a)
+    model = Convolution2D(num_channels, 3, 3, border_mode='same')(a)
     model = Activation('relu')(model)
     residual_block = model
     for i in range(num_res_blocks):
         # getting res block after addition to input
         residual_block = resblock(residual_block, num_channels)
     # adding model before res blocks, to output of all chain
-    # TODO: check if merge/Add is the correct method for adding 2 tensors
     model = merge([model, residual_block], mode='sum')
-    model = Convolution2D(1, 3, 3, border_mode='same', dim_ordering='th')(model)
+    model = Convolution2D(1, 3, 3, border_mode='same')(model)
     return Model(input=a, output=model)
 
 
@@ -128,7 +108,6 @@ def train_model(model, images, corruption_func, batch_size,
     :param samples_per_epoch: The number of samples per epochs
     :param num_epochs: The number of epochs to use when training the model
     :param num_valid_samples: The number of samples to use in validation phase
-    :return: A trained model
     """
     # splitting the images to 80-20 batches
     valid_pts = np.random.choice(len(images), int(np.floor(0.8 * len(images))))
@@ -136,11 +115,9 @@ def train_model(model, images, corruption_func, batch_size,
     images = np.array(images)
     test_generator = load_dataset(images[test_pts], batch_size, corruption_func, (int(model.inputs[0].shape[2]), int(model.inputs[0].shape[3])))
     valid_generator = load_dataset(images[valid_pts], batch_size, corruption_func, (int(model.inputs[0].shape[2]), int(model.inputs[0].shape[3])))
-    Adam(beta_2=0.9)
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.compile(optimizer=Adam(beta_2=0.9), loss='mean_squared_error')
     model.fit_generator(test_generator, samples_per_epoch, validation_data=valid_generator, nb_epoch=num_epochs,
                         nb_val_samples=num_valid_samples)
-    return model
 
 
 def add_motion_blur(image, kernel_size, angle):
@@ -152,7 +129,7 @@ def add_motion_blur(image, kernel_size, angle):
     :return: a blurred picture
     """
     kernel = sol5_utils.motion_blur_kernel(kernel_size, angle)
-    return ndimage.filters.convolve(image, kernel, mode='nearest')
+    return ndimage.filters.convolve(image, kernel)
 
 
 def random_motion_blur(image, list_of_kernel_sizes):
@@ -179,9 +156,10 @@ def learn_deblurring_model(num_res_blocks=5, quick_mode=False):
     model = build_nn_model(16, 16, 32, num_res_blocks)
     lambda_blur = lambda x: random_motion_blur(x, [7])
     if quick_mode:
-        return train_model(model, images, lambda_blur, 10, 30, 2, 30)
+        train_model(model, images, lambda_blur, 10, 30, 2, 30)
     else:
-        return train_model(model, images, lambda_blur, 100, 10000, 10, 1000)
+        train_model(model, images, lambda_blur, 100, 10000, 10, 1000)
+    return model
 
 
 def add_gaussian_noise(image, min_sigma, max_sigma):
@@ -194,10 +172,8 @@ def add_gaussian_noise(image, min_sigma, max_sigma):
     """
     div = random.uniform(min_sigma, max_sigma)
     # getting the normal noise, streching both the noise and the picture to values between 0-1
-    normal_noise = strech_helper(np.random.normal(0, scale=div, size=(image.shape[0], image.shape[1])))
-    image = strech_helper(image)
-    # TODO: remove division by 2, check if image is still corrupted in a good way
-    noisy_img = (normal_noise + image) / 2
+    normal_noise = np.random.normal(0, scale=div, size=(image.shape[0], image.shape[1])).astype(np.float32)
+    noisy_img = normal_noise + image
     return np.clip(noisy_img, 0, 1)
 
 
@@ -212,9 +188,10 @@ def learn_denoising_model(num_res_blocks=5, quick_mode=False):
     model = build_nn_model(24, 24, 48, num_res_blocks)
     lambda_noise = lambda x: add_gaussian_noise(x, 0, 0.2)
     if quick_mode:
-        return train_model(model, images, lambda_noise, 10, 30, 2, 30)
+        train_model(model, images, lambda_noise, 10, 30, 2, 30)
     else:
-        return train_model(model, images, lambda_noise, 100, 10000, 5, 1000)
+        train_model(model, images, lambda_noise, 100, 10000, 5, 1000)
+    return model
 
 
 def restore_image(corrupted_image, base_model):
@@ -227,34 +204,7 @@ def restore_image(corrupted_image, base_model):
     a = Input(shape=(1, corrupted_image.shape[0], corrupted_image.shape[1]))
     b = base_model(a)
     new_model = Model(input=a, output=b)
-    im = new_model.predict(np.reshape(np.array([corrupted_image]), (1, 1, corrupted_image.shape[0], corrupted_image.shape[1])))[0][0]
-    return im
-
-
-images = sol5_utils.images_for_deblurring()
-# denoise_model = learn_denoising_model()
-# deblur_model = learn_deblurring_model()
-
-denoise_model = build_nn_model(24, 24, 48, 5)
-deblur_model = build_nn_model(16, 16, 32, 5)
-
-denoise_model.load_weights("C:\/Users\Imri\PycharmProjects\IP_ex5\ex5-imrilu\denoise_model")
-deblur_model.load_weights("C:\/Users\Imri\PycharmProjects\IP_ex5\ex5-imrilu\deblur_model")
-
-im = read_image(images[16], 1)
-
-# showing original picture
-plt.imshow(im, cmap='gray')
-plt.show()
-
-im_corrupt = random_motion_blur(im, [7])
-im_corrupt2 = add_gaussian_noise(im, 0, 0.2)
-# showing corrupted image47
-plt.imshow(im_corrupt2, cmap='gray')
-plt.show()
-
-after = restore_image(im_corrupt2, denoise_model)
-# showing the picture after repair
-plt.imshow(after, cmap='gray')
-plt.show()
-
+    corrupted_image = corrupted_image[np.newaxis, np.newaxis, :, :] - 0.5
+    restored_img = new_model.predict(corrupted_image)[0].astype(np.float64)
+    restored_img += 0.5
+    return restored_img.clip(0, 1).reshape(corrupted_image.shape[2], corrupted_image.shape[3])
